da6e112df32fa903a6845127bbcbdaed26212fe7
kinggora <jhha09@ajou.ac.kr>
2022-05-10 00:10:29 +0900

    JPA와 DB 설정, 동작확인

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+		String str = new String("str");
+
 	}
 
 }
